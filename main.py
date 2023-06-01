ef32ec376ce2e457858ef07df55fc7845457908d
qgarot <quentingarot06@gmail.com>
2023-06-02 00:56:22 +0200

    Refactor main

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from database.database import Database
 from build_network.map import Map
-from pathfinder.road_network import RoadNetwork, Node, Point
-from matplotlib import pyplot as plt
+from pathfinder.road_network import RoadNetwork, Point
 from route_manager import RouteRequest, GondolaManager
 
 db = Database("localhost", "root", "", "tipe_ville")
 city = Map("build_network/img/map3.png", db)
+
 # Permet de définir les stations (les noeuds)
 # city.display_interactive_map()
 city.display_map_with_stations()
@@ -26,9 +26,8 @@ city.display_graph()
 network = RoadNetwork(city.get_stations(), city.get_adjacency_matrix())
 gondola_manager = GondolaManager(city)
 
-#  network.compare(1, 6, [Point.get_manhattan_distance, Point.get_euclidian_distance, Point.heuristic_null], ["#34ace0", "#33d9b2", "#ffb142"])
+# network.compare(1, 6, [Point.get_manhattan_distance, Point.get_euclidian_distance, Point.heuristic_null], ["#34ace0", "#33d9b2", "#ffb142"])
 
 request = RouteRequest((413, 454), (604, 744))
 print(request.get_route_data_str(network))
 gondola_manager.handle_request(request.get_route_data(network), network)
-# print(network.path_weight([network.get_node_by_id(2), network.get_node_by_id(3), network.get_node_by_id(4), network.get_node_by_id(7)]))
